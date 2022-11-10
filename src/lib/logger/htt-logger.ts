e7391057212778dcd2dfae9d11fb70b9a282b3ee
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-11-10 20:46:42 -0300

    feat: logger almost done

--- src/lib/logger/htt-logger.ts
+++ src/lib/logger/htt-logger.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { IError } from "@lib/errors";
 import { NextFunction, Request, Response } from "express";
 
 import wiston from "winston";
 
 type IExpressReqResLoggerParams = { logger: wiston.Logger };
 
-type IExpressResponseLoggerParams = (
+type IExpressResponseLoggerResponse = (
   req: Request,
   res: Response,
   next: NextFunction
@@ -16,3 +18,57 @@ type IExpressErrorLogger = (
   _res: Response,
   _: NextFunction
 ) => Response;
+
+type IHttpLogger = {
+  req_logger: (
+    param: IExpressReqResLoggerParams
+  ) => IExpressResponseLoggerResponse;
+  err_logger: (param: IExpressReqResLoggerParams) => IExpressErrorLogger;
+};
+
+function expressRequestLogger(
+  opts: IExpressReqResLoggerParams
+): IExpressResponseLoggerResponse {
+  const { logger } = opts;
+
+  return (req: Request, res: Response, next: NextFunction): void => {
+    function onResDone(_err: Error) {
+      res.removeListener("finish", onResDone);
+      res.removeListener("error", onResDone);
+    }
+
+    logger.info(
+      `handled ${req.method} ${req.path}. Body ${JSON.stringify(req.body)}`
+    );
+
+    res.on("finish", onResDone);
+    res.on("error", onResDone);
+
+    next();
+  };
+}
+
+function expressErrorLogger(
+  opts: IExpressReqResLoggerParams
+): IExpressErrorLogger {
+  return (
+    err: IError,
+    _req: Request,
+    _res: Response,
+    _: NextFunction
+  ): Response => {
+    return _res.status(err.status || 500).json({
+      error: {
+        errorType: err.type,
+        message: err.message,
+      },
+    });
+  };
+}
+
+const httpLogger = {
+  err_logger: expressErrorLogger,
+  req_logger: expressRequestLogger,
+} as IHttpLogger;
+
+export default httpLogger;
